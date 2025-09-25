800dcbd6af5a66f58a33cd76a19bb8f5a0ec17a2
2300031236 <2300031236@kluniversity.in>
2025-09-25 20:16:12 +0530

    V1.0.2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,18 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './Home.jsx';
-import Signup from './Signup.jsx';
-import Login from './Login.jsx';
-import Dashboard from './Dashboard.jsx';
-import './Styles.css';
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home.jsx";
+import Signup from "./Signup.jsx";
+import Login from "./Login.jsx";
+import Dashboard from "./Dashboard.jsx";
+import "./Styles.css";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
   );
 }
 
